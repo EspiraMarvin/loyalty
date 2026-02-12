6d08453d715fc90fa7ac98f3cc01221efb4493e2
EspiraMarvin <espiramarvin@gmail.com>
2026-02-12 17:41:59 +0300

    feat: add graphql module defitionn & config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OffersModule } from './offers/offers.module';
@@ -8,6 +11,15 @@ import { OffersModule } from './offers/offers.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      context: ({ req }) => ({ req }),
     }),
     OffersModule,
   ],
