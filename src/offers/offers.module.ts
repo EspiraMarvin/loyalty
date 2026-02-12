61c43ef0333a3c879ceb37d41ddc6cc434329154
EspiraMarvin <espiramarvin@gmail.com>
2026-02-12 17:40:53 +0300

    feat: add offers module & resolver

--- src/offers/offers.module.ts
+++ src/offers/offers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OffersService } from './offers.service';
 import { OffersResolver } from './offers.resolver';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [OffersResolver, OffersService],
 })
 export class OffersModule {}
--- src/offers/offers.resolver.ts
+++ src/offers/offers.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { OffersService } from './offers.service';
+import { OfferSummaryType } from './types/offer-summary.type';
+import { OffersFilterInput } from './dto/offers-filter.input';
 
 @Resolver()
-export class OffersResolver {}
+export class OffersResolver {
+  constructor(private readonly offersService: OffersService) {}
+
+  @Query(() => OfferSummaryType, {
+    name: 'offers',
+    description: 'Get all available offers filtered by various criteria',
+  })
+  async getOffers(
+    @Args('filter', { type: () => OffersFilterInput, nullable: true })
+    filter?: OffersFilterInput,
+  ): Promise<OfferSummaryType> {
+    return (await this.offersService.getOffers(filter || {})) as any;
+  }
+}
